81f302567aa88a5ce7be8153c95665a03693842a
consultheron <consultheron@gmail.com>
2023-05-18 06:59:13 -0500

    Added routing and navigation, some style
    Signed-off-by: consultheron <consultheron@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import MainMenu from "./components/MainMenu";
-import EventList from "./components/EventList";
+import { Route, Switch } from "react-router-dom";
+// import MainMenu from "./components/MainMenu";
+
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetups from "./pages/NewMeetups";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 
 
 function App() {
   return (
     <div>
-      <MainMenu/>
-      <EventList />
+      <MainNavigation />
+      <Switch>
+        {/* <MainMenu /> */}
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/newmeetups">
+          <NewMeetups />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
