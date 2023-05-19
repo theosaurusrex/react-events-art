3363891b1b71acb168ecbf60c92c47a189fca65b
consultheron <consultheron@gmail.com>
2023-05-18 21:18:07 -0500

    Added styles, UI components, layout
    Signed-off-by: consultheron <consultheron@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import { Route, Switch } from "react-router-dom";
-// import MainMenu from "./components/MainMenu";
-
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetups from "./pages/NewMeetups";
 import Favorites from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
-
-
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        {/* <MainMenu /> */}
-        <Route path="/" exact>
-          <AllMeetups />
-        </Route>
-        <Route path="/newmeetups">
-          <NewMeetups />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetups />
+          </Route>
+          <Route path="/newmeetups">
+            <NewMeetups />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
